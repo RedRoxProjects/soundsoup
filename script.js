81222b1268e8d790239f38a253972c52702da898
RedRoxProjects <psxad2@nottingham.ac.uk>
2018-11-11 08:39:21 +0000

    works on mobile

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const FUNDAMENTAL = 220;
 
 // Create an array of harmonics of the fundamental
-var frequencyList = [FUNDAMENTAL, (FUNDAMENTAL * 2), (FUNDAMENTAL * 3), (FUNDAMENTAL * 4), (FUNDAMENTAL * 5), (FUNDAMENTAL * 6), (FUNDAMENTAL * 7), (FUNDAMENTAL * 8), (FUNDAMENTAL * 9), (FUNDAMENTAL * 10), (FUNDAMENTAL * 11), (FUNDAMENTAL * 12), (FUNDAMENTAL * 13)];
+const frequencyList = [FUNDAMENTAL, (FUNDAMENTAL * 2), (FUNDAMENTAL * 3), (FUNDAMENTAL * 4), (FUNDAMENTAL * 5), (FUNDAMENTAL * 6), (FUNDAMENTAL * 7), (FUNDAMENTAL * 8), (FUNDAMENTAL * 9), (FUNDAMENTAL * 10), (FUNDAMENTAL * 11), (FUNDAMENTAL * 12), (FUNDAMENTAL * 13)];
 console.log(frequencyList);
 
 // Load frequency at random from array
-var harmonic = frequencyList[Math.floor(Math.random() * frequencyList.length)]
+const harmonic = frequencyList[Math.floor(Math.random() * frequencyList.length)]
 console.log(harmonic);
 
 // Set up Audio Context
-var audioContext = new AudioContext(); 
+const audioContext = new AudioContext(); 
 
 // Set up Oscillator
-var osc = audioContext.createOscillator();
+const osc = audioContext.createOscillator();
 
 // Set up oscillator frequency
 osc.frequency.value = harmonic;
@@ -32,3 +32,16 @@ function stop() {
     osc.disconnect(audioContext.destination);
 }
 
+function endTapOrClick(event) {
+    //handle tap or click.
+    stop();
+    event.preventDefault();
+    return false;
+}
+
+function TapOrClick(event) {
+    //handle tap or click.
+    play();
+    event.preventDefault();
+    return false;
+}
